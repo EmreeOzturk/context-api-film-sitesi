ffcbc3a07f70150ff51745e250740ffab12b7927
Emre Ozturk <emreeozturk42@gmail.com>
2023-01-08 05:42:13 +0300

    banner added to add page's image

--- pages/add.jsx
+++ pages/add.jsx
@@ -8,6 +8,7 @@ const add = () => {
           src="https://www.themoviedb.org/t/p/w1920_and_h600_multi_faces_filter(duotone,032541,01b4e4)/9ZyAUZrfccsjtDwYgc7yvOBnqM9.jpg"
           width={1920}
           height={600}
+          alt="banner"
         />
       </div>
     </div>
